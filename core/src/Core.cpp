fa1102a336074d2d8a17f6837b23cdb39bc4092e
fabio-terranova <fabio.terranova@outlook.it>
2026-01-09 00:10:22 +0100

    style: indentation

--- core/src/Core.cpp
+++ core/src/Core.cpp
@@ -3,76 +3,76 @@
 #include <vector>
 
 namespace Noddy {
-  namespace Core {
-    std::string getCoreVersion() { return "Noddy Core v0.0.1"; }
+namespace Core {
+std::string getCoreVersion() { return "Noddy Core v0.0.1"; }
 
-    using SignalMatrix = Eigen::Matrix<double, 2, 2>;
+using SignalMatrix = Eigen::Matrix<double, 2, 2>;
 
-    struct Signal {
-      SignalMatrix data;
-      float        fs;
-    };
+struct Signal {
+  SignalMatrix data;
+  float        fs;
+};
 
-    class Node {
-    public:
-      virtual ~Node() = default;
+class Node {
+public:
+  virtual ~Node() = default;
 
-      virtual Signal process(const Signal& input) = 0;
-    };
+  virtual Signal process(const Signal& input) = 0;
+};
 
-    class ForwardNode final : public Node {
-    public:
-      Signal process(const Signal& input) { return input; }
-    };
+class ForwardNode final : public Node {
+public:
+  Signal process(const Signal& input) { return input; }
+};
 
-    class DoubleNode final : public Node {
-    public:
-      Signal process(const Signal& input) {
-        return Signal{input.data * 2, input.fs};
-      }
-    };
+class DoubleNode final : public Node {
+public:
+  Signal process(const Signal& input) {
+    return Signal{input.data * 2, input.fs};
+  }
+};
 
-    class SqrtNode final : public Node {
-    public:
-      Signal process(const Signal& input) {
-        return Signal{input.data.cwiseAbs().cwiseSqrt(), input.fs};
-      }
-    };
+class SqrtNode final : public Node {
+public:
+  Signal process(const Signal& input) {
+    return Signal{input.data.cwiseAbs().cwiseSqrt(), input.fs};
+  }
+};
 
-    using NodeRefsVector = std::vector<std::reference_wrapper<Node>>;
+using NodeRefsVector = std::vector<std::reference_wrapper<Node>>;
 
-    class Graph {
-    public:
-      Graph(const NodeRefsVector nodes) : nodes_{nodes} {}
+class Graph {
+public:
+  Graph(const NodeRefsVector nodes) : nodes_{nodes} {}
 
-      void   addNode(Node& node);
-      Signal execute(const Signal& input);
+  void   addNode(Node& node);
+  Signal execute(const Signal& input);
 
-    private:
-      NodeRefsVector nodes_;
-    };
+private:
+  NodeRefsVector nodes_;
+};
 
-    void Graph::addNode(Node& node) { nodes_.push_back(node); }
+void Graph::addNode(Node& node) { nodes_.push_back(node); }
 
-    Signal Graph::execute(const Signal& input) {
-      Signal current{input};
-      for (auto node : nodes_) {
-        current = node.get().process(current);
-      }
+Signal Graph::execute(const Signal& input) {
+  Signal current{input};
+  for (auto node : nodes_) {
+    current = node.get().process(current);
+  }
 
-      return current;
-    }
+  return current;
+}
 
-    class Pipeline {
-    public:
-      Pipeline(Graph graph) : graph_{graph} {}
+class Pipeline {
+public:
+  Pipeline(Graph graph) : graph_{graph} {}
 
-      Pipeline bandpass(double low, double high) const;
+  Pipeline bandpass(double low, double high) const;
 
-      Signal run(const Signal& input) const;
+  Signal run(const Signal& input) const;
 
-    private:
-      Graph graph_;
-    };
-  } // namespace Core
+private:
+  Graph graph_;
+};
+} // namespace Core
 } // namespace Noddy
