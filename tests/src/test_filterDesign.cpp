3a2584fb65ddabde0ebdc5379b3fe71a6d6b4592
fabio-terranova <fabio.terranova@outlook.it>
2026-01-09 17:09:32 +0100

    fix: update filter design tests to use order 7 and measure elapsed time

--- tests/src/test_filterDesign.cpp
+++ tests/src/test_filterDesign.cpp
@@ -32,33 +32,43 @@ std::ostream& operator<<(std::ostream& os, const ZPK& zpk) {
 bool test_filterDesign() {
   using namespace Noddy::Filter;
 
-  const auto order{2};
+  const auto order{7};
   const auto fc{100.0};
   const auto fs{1000.0};
   const auto ftype{lowpass};
 
+  Timer timer{};
   { // Butterworth
-    ZPK digitalFilter{iirFilter<buttap, ftype>(order, fc, fs)};
+    timer.reset();
+    ZPK  digitalFilter{iirFilter<buttap, ftype>(order, fc, fs)};
+    auto elapsed{timer.elapsed()};
 
     std::cout << "--- Design Butterworth filter ---" << '\n';
+    std::cout << "Time taken: " << elapsed << '\n';
     std::cout << digitalFilter << '\n';
   }
 
   { // Chebyshev I
     const auto rp{5.0};
 
-    ZPK digitalFilter{iirFilter<cheb1ap, ftype>(order, fc, fs, rp)};
+    timer.reset();
+    ZPK  digitalFilter{iirFilter<cheb1ap, ftype>(order, fc, fs, rp)};
+    auto elapsed{timer.elapsed()};
 
     std::cout << "--- Design Chebyshev I filter ---" << '\n';
+    std::cout << "Time taken: " << elapsed << '\n';
     std::cout << digitalFilter << '\n';
   }
 
   { // Chebyshev II
     const auto rs{4.0};
 
-    ZPK digitalFilter{iirFilter<cheb2ap, ftype>(order, fc, fs, rs)};
+    timer.reset();
+    ZPK  digitalFilter{iirFilter<cheb2ap, ftype>(order, fc, fs, rs)};
+    auto elapsed{timer.elapsed()};
 
     std::cout << "--- Design Chebyshev II filter ---" << '\n';
+    std::cout << "Time taken: " << elapsed << '\n';
     std::cout << digitalFilter << '\n';
   }
 
